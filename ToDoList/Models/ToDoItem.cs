230de14054c0d8d4d8f84ea4742fceed4b337d23
Omer Latif <olatif@u.rochester.edu>
2019-08-23 13:00:18 -0400

    pushed all code to initial

---
+++ ToDoList/Models/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
